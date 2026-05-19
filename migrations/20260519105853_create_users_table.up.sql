2fb5d8f4bea8370be3aa25a311e736b2d2819b47
AltheaIX <althea.ixy@gmail.com>
2026-05-19 18:04:16 +0700

    feat(infra): add migration users schema

---
+++ migrations/20260519105853_create_users_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE INDEX idx_users_email ON users(email);
