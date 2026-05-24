251279e4206e1825dea6751648071629752e8c59
AltheaIX <althea.ixy@gmail.com>
2026-05-24 22:55:47 +0700

    update(api): add stack trace on panic recovery

--- internal/transport/middleware/recovery.go
+++ internal/transport/middleware/recovery.go
@@ -1,20 +1,34 @@
 package middleware
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
+	"runtime/debug"
+
+	"github.com/Highload-Labs/healthcare-gov-backend/internal/config"
 )
 
 func RecoveryMiddleware(next http.Handler) http.Handler {
+	cfg := config.GetConfig()
+
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			defer func() {
 				if err := recover(); err != nil {
-					slog.Info(
-						"panic recovered", err,
-						"method", r.Method,
-						"path", r.URL.Path,
-					)
+					stackTrace := debug.Stack()
+					if cfg.GoEnv == "development" {
+						slog.Error("panic recovered", "error", err, "method", r.Method, "path", r.URL.Path)
+						fmt.Printf("\n--- STACK TRACE ---\n%s\n-------------------\n", stackTrace)
+					} else {
+						slog.Error(
+							"panic recovered",
+							"error", err,
+							"method", r.Method,
+							"path", r.URL.Path,
+							"stack", string(stackTrace),
+						)
+					}
 
 					w.WriteHeader(http.StatusInternalServerError)
 				}
